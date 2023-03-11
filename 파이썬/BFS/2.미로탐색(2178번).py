f47f25af3f68967088e8b0ec5b41da246448da02
Kang Yonghyeon <109471063+kyhyeon0302@users.noreply.github.com>
2023-03-11 17:19:36 +0900

    Update 2.미로탐색(2178번).py

--- 파이썬/BFS/2.미로탐색(2178번).py
+++ 파이썬/BFS/2.미로탐색(2178번).py
@@ -1,31 +1,36 @@
 from collections import deque
 
 N, M = map(int, input().split())
+visit = [[False] * M for i in range(N)]
 
-maze = []
+graph = []
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
 for i in range(N):
-    maze.append(list(map(int, input())))
+    graph.append(list(map(int, input())))
+
 
 def bfs(x, y):
     q = deque()
     q.append((x, y))
 
+
     while q:
         x, y = q.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0<= nx <N and 0<= ny <M :
-                if maze[nx][ny] == 1:
-                    maze[nx][ny] = maze[x][y] + 1
+            if 0<=nx<N and 0<=ny<M:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = graph[x][y] + 1
                     q.append((nx, ny))
 
-    return maze[N-1][M-1]
 
 
-print(bfs(0, 0))
 
+bfs(0, 0)
+
+print(graph[N-1][M-1])
